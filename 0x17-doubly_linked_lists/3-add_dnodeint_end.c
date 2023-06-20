48dab96244fadea38d52bab7b2aea92179b60910
LaylaZaya <layjoon@gmail.com>
2023-06-20 06:50:19 -0700

    linked

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,38 +1,39 @@
 #include "lists.h"
-#include <string.h>
-#include <stdio.h>
 
 /**
- * add_dnodeint_end - add a node at the end of a linked list
- * @head: The character to print
- * @n: string for the new node
+ * add_dnodeint_end - adds a new node at the end
+ * of a dlistint_t list
  *
- * Return: new node
+ * @head: head of the list
+ * @n: value of the element
+ * Return: the address of the new element
  */
-
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *current;
-	dlistint_t *new_node;
+    dlistint_t *h;
+    dlistint_t *new;
 
-	current = *head;
-	while (current && current->next != NULL)
-		current = current->next;
+    new = malloc(sizeof(dlistint_t));
+    if (new == NULL)
+        return (NULL);
 
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->n = n;
-	new_node->next = NULL;
-    new_node->prev = current;
+    new->n = n;
+    new->next = NULL;
 
-	if (current)
-		current->next = new_node;
-	else
-		*head = new_node;
+    h = *head;
 
-	return (new_node);
+    if (h != NULL)
+    {
+        while (h->next != NULL)
+            h = h->next;
+        h->next = new;
+    }
+    else
+    {
+        *head = new;
+    }
+
+    new->prev = h;
+
+    return (new);
 }
--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,41 +1,53 @@
+c
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - delete node at specific index
- * @head: head of linked list
- * @index: index of node to delete
- * Return: 1 on success, -1 on failure
+ * delete_dnodeint_at_index - deletes the node at index of a
+ * dlistint_t linked list
+ *
+ * @head: head of the list
+ * @index: index of the new node
+ * Return: 1 if it succeeded, -1 if it failed
  */
-
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *current;
+    dlistint_t *h1;
+    dlistint_t *h2;
+    unsigned int i;
 
-	if (head == NULL)
-		return (-1);
+    h1 = *head;
 
-	current = *head;
+    if (h1 != NULL)
+        while (h1->prev != NULL)
+            h1 = h1->prev;
 
-	if (current == NULL)
-		return (-1);
+    i = 0;
 
-	while (index != 0)
-	{
-		current = current->next;
-		index--;
-		if (current == NULL)
-			return (-1);
-	}
+    while (h1 != NULL)
+    {
+        if (i == index)
+        {
+            if (i == 0)
+            {
+                *head = h1->next;
+                if (*head != NULL)
+                    (*head)->prev = NULL;
+            }
+            else
+            {
+                h2->next = h1->next;
 
-	if (current->prev != NULL)
-		current->prev->next = current->next;
-	else
-	{
-		*head = current->next;
-		if (current->next != NULL)
-			current->next->prev = NULL;
-		free(current);
-	}
+                if (h1->next != NULL)
+                    h1->next->prev = h2;
+            }
 
-	return (1);
+            free(h1);
+            return (1);
+        }
+        h2 = h1;
+        h1 = h1->next;
+        i++;
+    }
+
+    return (-1);
 }
